455743e1de38f1dda2da918c7cf24a7fe83ef6b6
20k <the_berronator@hotmail.co.uk>
2016-02-27 03:49:22 +0000

    fun with auto find

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ bool once()
 
 typedef vec<2, double> vec2d;
 
-double dt_s = 12000;
+double dt_s = 12000 / 5;
 double dt_old = dt_s;
 
 
@@ -70,6 +70,8 @@ struct orbital
 
     vec2d acc = {0,0};
 
+    double radius;
+
     void set_speed(double speed)
     {
         old_pos = perpendicular(pos).norm() * speed * pow(dt_s, 1.) + pos;
@@ -81,7 +83,7 @@ struct orbital
     }
 
     ///amount in ms
-    void accelerate_relative_to_velocity(double amount, float angle_offset, float time_s)
+    void accelerate_relative_to_velocity(double amount, double angle_offset, double time_s)
     {
         vec2d vel_dir = (pos - old_pos).norm();
 
@@ -89,12 +91,14 @@ struct orbital
         acc = acc + vel_dir.rot(angle_offset) * amount * time_s * time_s;
     }
 
-    orbital(double _mass, double _distance, double _speed)
+    orbital(double _mass, double _distance, double _speed, double rad = 0)
     {
         mass = _mass;
         pos = {_distance, 0};
 
         set_speed(_speed);
+
+        radius = rad;
     }
 
     orbital()
@@ -269,7 +273,7 @@ struct manager
             display(tex, 1);
         }
 
-        for(int i=0; i<olist.size(); i++)
+        for(int i=0; i<(int)olist.size(); i++)
         {
             *olist[i] = old[i];
         }
@@ -277,7 +281,7 @@ struct manager
         tex.display();
     }
 
-    vector<vector<vec2d>> test(int ticks, sf::RenderWindow& tex, bool render = true, orbital* test_orbital = nullptr, std::vector<orbital*> info_to_retrieve = std::vector<orbital*>())
+    vector<vector<vec2d>> test(int ticks, sf::RenderWindow* tex, bool render = true, orbital* test_orbital = nullptr, std::vector<orbital*> info_to_retrieve = std::vector<orbital*>())
     {
         std::vector<orbital> old;
 
@@ -308,12 +312,12 @@ struct manager
             tick();
 
             if(render)
-                display(tex);
+                display(*tex);
 
             if(i % 1 == 0 && render)
             {
-                tex.display();
-                tex.clear();
+                tex->display();
+                tex->clear();
             }
 
             if(test_orbital)
@@ -344,6 +348,142 @@ struct manager
         }
 
         return test_ret;
+    }
+
+    ///returns the tick where the two objects are closest
+    /*int get_minimum_distance(int ticks, orbital* test_orbital, orbital* target_orbital)
+    {
+        vector<vector<vec2d>> result = test(ticks, nullptr, false, test_orbital, {target_orbital});
+
+        int min_tick = -1;
+
+        double min_len = DBL_MAX;
+
+        for(int tnum=0; tnum<result[0].size(); tnum++)
+        {
+            ///test
+            vec2d d1 = result[0][tnum];
+            ///target
+            vec2d d2 = result[1][tnum];
+
+            if((d1 - d2).length() < min_len)
+            {
+                min_len = (d1 - d2).length();
+                min_tick = tnum;
+            }
+        }
+
+        return min_tick;
+    }*/
+
+    int get_minimum_distance(int o1, int o2, const vector<vector<vec2d>>& pos)
+    {
+        int min_tick = -1;
+
+        double min_len = DBL_MAX;
+
+        for(int tnum=0; tnum<pos[o1].size(); tnum++)
+        {
+            ///test
+            vec2d d1 = pos[o1][tnum];
+            ///target
+            vec2d d2 = pos[o2][tnum];
+
+            if((d1 - d2).length() < min_len)
+            {
+                min_len = (d1 - d2).length();
+                min_tick = tnum;
+            }
+        }
+
+        return min_tick;
+    }
+
+    struct ret_info
+    {
+        vector<vector<vec2d>> ret;
+        int mtick;
+    };
+
+    ret_info bisect(int ticks, float base_speed, float minimum, float maximum, int num_per_step, int depth, orbital* test_orbital = nullptr, orbital* target_orbital = nullptr, std::vector<orbital*> info_to_retrieve = std::vector<orbital*>(), int c = 0)
+    {
+        int which_id = -1;
+
+        for(int i=0; i<info_to_retrieve.size(); i++)
+        {
+            if(info_to_retrieve[i] == target_orbital)
+            {
+                which_id = i + 1;
+            }
+        }
+
+        ///add target orbital if its not found
+        if(which_id == -1)
+        {
+            info_to_retrieve.insert(info_to_retrieve.begin(), target_orbital);
+
+            //info_to_retrieve.push_back(target_orbital);
+            //which_id = info_to_retrieve.size();
+
+            which_id = 1;
+        }
+
+        double step = (maximum - minimum) / num_per_step;
+
+        double min_min = DBL_MAX;
+        int saved_mtick = -1;
+        double found_speed = 1;
+        double found_mod = minimum;
+
+        vector<vector<vec2d>> backup;
+
+        for(int i=0; i<num_per_step; i++)
+        {
+            orbital probe = *test_orbital;
+
+            float ifrac = (float)i / num_per_step;
+
+            double cur = (maximum - minimum) * ifrac + minimum;
+
+            double speed = base_speed / cur;
+
+
+            probe.accelerate_relative_to_velocity(1.f, 0, speed);
+
+
+            auto last_experiment = this->test(ticks, nullptr, false, &probe, info_to_retrieve);
+
+            int mtick = this->get_minimum_distance(0, which_id, last_experiment);
+
+
+            vec2d min_voyager = last_experiment[0][mtick];
+            vec2d min_mars = last_experiment[which_id][mtick];
+
+            if((min_mars - min_voyager).length() < target_orbital->radius)
+            {
+                printf("HIT\n");
+            }
+
+            if((min_mars - min_voyager).length() < min_min)
+            {
+                min_min = (min_mars - min_voyager).length();
+                //f_acc = cur;
+                backup = last_experiment;
+                saved_mtick = mtick;
+                found_speed = speed;
+                found_mod = cur;
+            }
+        }
+
+        printf("found_acc %f\n", found_mod);
+        printf("found mindist %f\n", min_min / 1000 / 1000 / 1000);
+
+        if(c+1 < depth)
+        {
+            return bisect(ticks, base_speed, found_mod - step/2, found_mod + step/2, num_per_step, depth, test_orbital, target_orbital, info_to_retrieve, c+1);
+        }
+
+        return {backup, saved_mtick};
     }
 
     void plot(const vector<vector<vec2d>>& elements, int which_element, int which_tick, sf::RenderWindow& win, vec3f col = {1, 1, 1})
@@ -385,7 +525,7 @@ int main()
     sun->col = {1.f, 0.8, 0.4};
 
 
-    orbital* earth = orbital_manager.make_new(orbital(5.962 * pow(10., 24.), 149.6 * pow(10, 9), 30. * pow(10, 3)));
+    orbital* earth = orbital_manager.make_new(orbital(5.962 * pow(10., 24.), 149.6 * pow(10, 9), 30. * pow(10, 3), 6371 * pow(10, 3)));
 
     earth->col = {0.2f, 0.2f, 1.f};
 
@@ -403,7 +543,7 @@ int main()
 
     orbital* venus = orbital_manager.make_new(orbital(4.87 * pow(10, 24), 108.2 * pow(10, 9), 35.02 * pow(10, 3)));
 
-    orbital* mars = orbital_manager.make_new(orbital(6.42 * pow(10, 23), 227.9 * pow(10, 9), 24.077 * pow(10, 3)));
+    orbital* mars = orbital_manager.make_new(orbital(6.42 * pow(10, 23), 227.9 * pow(10, 9), 24.077 * pow(10, 3), 3390 * pow(10, 3)));
 
 
     orbital* jupiter = orbital_manager.make_new(orbital(1.9 * pow(10, 27), 778.3 * pow(10, 9), 13.07 * pow(10, 3)));
@@ -426,7 +566,7 @@ int main()
 
     ///earth->speed = 30km/s
 
-    vec2d start = 0;
+    //vec2d start = 0;
 
     float day = 0;
 
@@ -491,33 +631,131 @@ int main()
 
         vec2d world_p = manager::screen2pos(m, wh);
 
-        if(once<sf::Mouse::Left>())
+        /*if(once<sf::Mouse::Left>())
         {
             orbital* getrekd = orbital_manager.make_new();
             getrekd->mass = pow(10., 3.);
             getrekd->pos = world_p;
             getrekd->set_speed(13.07 * pow(10, 3) * 10, {0, -1});
-        }
+        }*/
 
         if(once<sf::Mouse::Right>())
         {
             orbital voyager_probe = *earth;
             voyager_probe.mass = 721.9;
             voyager_probe.col = {1, 0, 0};
-            voyager_probe.accelerate_relative_to_velocity(1.f, 0, 12000 / 2);
 
-            last_experiment = orbital_manager.test(10000, win, false, &voyager_probe, {earth});
+            float base_speed = 12000;
+
+            /*voyager_probe.accelerate_relative_to_velocity(1.f, 0, base_speed / 1.5);
+
+            int tnum = 15000;
+
+            last_experiment = orbital_manager.test(tnum, &win, false, &voyager_probe, {earth, mars});*/
+
+            /*int tnum = 30000;
+
+            int test_nums = 100;
+
+
+            double min_min = DBL_MAX;
+            double f_acc = 0.;
+            int saved_mtick = -1;
+
+            //float min_acc = 1.0;
+            //float max_acc = 2.0;
+
+            float min_acc = 1.193;
+            float max_acc = 1.195;
+
+            vector<vector<vec2d>> backup;
+
+            for(int i=0; i<test_nums; i++)
+            {
+                orbital voyager_probe_this = voyager_probe;
+
+                float ifrac = (float)i / test_nums;
+
+                double cur = (max_acc - min_acc) * ifrac + min_acc;
+
+                double speed = base_speed / cur;
+
+
+                voyager_probe_this.accelerate_relative_to_velocity(1.f, 0, speed);
+
+
+                last_experiment = orbital_manager.test(tnum, &win, false, &voyager_probe_this, {earth, mars});
+
+                int mtick = orbital_manager.get_minimum_distance(0, 2, last_experiment);
+
+
+                vec2d min_voyager = last_experiment[0][mtick];
+                vec2d min_mars = last_experiment[2][mtick];
+
+                if((min_mars - min_voyager).length() < mars->radius)
+                {
+                    printf("HIT\n");
+                }
+
+                if((min_mars - min_voyager).length() < min_min)
+                {
+                    min_min = (min_mars - min_voyager).length();
+                    f_acc = cur;
+                    backup = last_experiment;
+                    saved_mtick = mtick;
+                }
+            }
+
+            printf("%f min_min %f speed\n", min_min / 1000. / 1000. / 1000., f_acc);
+            printf("%f target radius in millionkm\n", mars->radius / 1000. / 1000. / 1000);*/
+
+            int tnum = 40000;
+
+            auto info = orbital_manager.bisect(tnum, 12000, 0.5, 5.0, 20, 5, &voyager_probe, mars, {earth, sun});
+
+            double min_dist = (info.ret[0][info.mtick] - info.ret[1][info.mtick]).length();
+
+            printf("min_dist %f\n", min_dist / 1000 / 1000 / 1000);
+
 
             ///tomorrow's homework
             ///shoot probes out in all directions, see which ones come closest to target
-            for(int i=0; i<10000; i++)
+            for(int i=0; i<tnum; i++)
             {
-                orbital_manager.plot_all(last_experiment, i, win, {{1, 0, 0}, earth->col});
+                orbital_manager.plot_all(info.ret, i, win, {{0, 1, 0}, {1, 0, 0}, earth->col, sun->col});
 
-                win.display();
 
-                win.clear();
+                if(info.mtick == i)
+                    system("pause");
+
+                int every = 2;
+
+                if(i % every == 0)
+                {
+                    win.display();
+                    win.clear();
+                }
             }
+
+
+
+            /*
+            ///voyager, mars
+            int min_tick = orbital_manager.get_minimum_distance(0, 2, last_experiment);
+
+            vec2d min_voyager = last_experiment[0][min_tick];
+            vec2d min_mars = last_experiment[2][min_tick];
+
+            if((min_mars - min_voyager).length() < mars->radius)
+            {
+                printf("HIT\n");
+            }
+
+            printf("min %i dist %f\n", min_tick, (min_mars - min_voyager).length() / 1000. / 1000. / 1000.);*/
+
+            //int min_tick = orbital_manager.get_minimum_distance(10000, &voyager_probe, &mars);
+
+            //printf("")
 
             //system("pause");
 
